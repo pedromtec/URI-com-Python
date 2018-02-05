8b7c6e5c368aa30020423bee81de3c3a7d55a5aa
Pedro Michael <pedromtec@gmail.com>
2018-02-05 14:23:01 -0300

    Atualizando Estruturas e Bibliotecas

--- Estruturas_e_Bibliotecas/1069.py
+++ Estruturas_e_Bibliotecas/1069.py
@@ -1,15 +1,18 @@
+#Diamantes e Areia
 n = int(input())
 for i in range(n):
-	entrada = input()
-	pilha = []
-	cont = 0
-	for c in entrada:
-		if c == '.':
-			continue
-		if c == '<':
-			pilha.append(c)
-		else:
-			if pilha:
-				pilha.pop()
-				cont+=1
-	print(cont)
+    entrada = input()
+    pilha = []
+    cont = 0
+    for c in entrada:
+        if c == '.':
+            continue
+        if c == '<':
+            pilha.append(c)
+        else:
+            if pilha:
+                pilha.pop()
+                cont+=1
+    print(cont)
+
+
