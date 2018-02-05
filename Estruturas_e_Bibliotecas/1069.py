24e1d83edc8a97f2732714d9c8e8f85ea684ac5b
Pedro Michael <pedromtec@gmail.com>
2018-02-05 14:14:19 -0300

    Atualizando Estruturas e Bibliotecas

---
+++ Estruturas_e_Bibliotecas/1069.py
@@ -0,0 +1,15 @@
+n = int(input())
+for i in range(n):
+	entrada = input()
+	pilha = []
+	cont = 0
+	for c in entrada:
+		if c == '.':
+			continue
+		if c == '<':
+			pilha.append(c)
+		else:
+			if pilha:
+				pilha.pop()
+				cont+=1
+	print(cont)
